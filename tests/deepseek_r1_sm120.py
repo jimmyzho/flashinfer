5fdca01770a6964f49013e3adbfa380359855288
jimmzhou <jimmzhou@nvidia.com>
2025-10-08 00:47:49 +0000

    update mla

--- tests/deepseek_r1_sm120.py
+++ tests/deepseek_r1_sm120.py
@@ -9,11 +9,12 @@ from flashinfer import (
     nvfp4_quantize,
     mxfp4_quantize,
 )
+import math
 import torch.nn.functional as F
 from flashinfer.utils import get_compute_capability, LibraryError
 
 # Layer: Context Attention
-# Operation: CUTLASS FMHA --> not available, use FA2 backend, only support bf16 input
+# Operation: CUTLASS FMHA --> not available, use FA2 backend, only support bf16 input, bf16, fp8 output
 # FMHA Paged KV Cache (non varlen)
 @pytest.mark.parametrize("batch_size", [1, 19, 99])
 @pytest.mark.parametrize("page_size", [1, 5])
@@ -138,8 +139,8 @@ def test_batch_ragged_prefill_packed_input(
 
     torch.testing.assert_close(o_packed, o_contiguous, rtol=1e-3, atol=1e-3)
 
-
-# Single non batched prefill, bf16 input only
+# Deprecated
+# Single non batched prefill, bf16 input only, bf16, fp8 output
 @pytest.mark.parametrize("seq_len", [1, 7, 127, 999, 3579])
 @pytest.mark.parametrize("num_kv_heads", [1, 4, 8])
 @pytest.mark.parametrize("num_qo_heads", [4, 8, 32])
@@ -153,7 +154,7 @@ def test_single_prefill_packed_input(
     qkv_packed = torch.randn(
         seq_len,
         (num_qo_heads + 2 * num_kv_heads) * head_dim,
-        dtype=torch.float8_e5m2,
+        dtype=torch.bfloat16,
         device="cuda:0",
     )
     q = qkv_packed[:, : num_qo_heads * head_dim].reshape(
@@ -166,178 +167,168 @@ def test_single_prefill_packed_input(
         seq_len, num_kv_heads, head_dim
     )
 
-    o_packed = flashinfer.single_prefill_with_kv_cache(q, k, v, causal=causal)
+    o_packed = flashinfer.single_prefill_with_kv_cache(q, k, v, causal=causal, o_dtype=torch.float8_e5m2)
     o_contiguous = flashinfer.single_prefill_with_kv_cache(
-        q.contiguous(), k.contiguous(), v.contiguous(), causal=causal
+        q.contiguous(), k.contiguous(), v.contiguous(), causal=causal, o_dtype=torch.float8_e5m2
     )
 
-    torch.testing.assert_close(o_packed, o_contiguous, rtol=1e-3, atol=1e-3)
+    # torch.testing.assert_close(o_packed, o_contiguous, rtol=1e-2, atol=1e-2)
+    torch.testing.assert_close(o_packed, o_contiguous, rtol=0.0, atol=0.0)
 
-# Layer: Decode MLA -> Not supported, use cudnn decode (only support bf16)
-@pytest.mark.parametrize("batch_size", [8, 16, 32])
-@pytest.mark.parametrize("s_kv", [512, 8192])
-@pytest.mark.parametrize("page_size", [16])
-@pytest.mark.parametrize("num_kv_heads", [8])
-@pytest.mark.parametrize("num_qo_heads", [32])
-@pytest.mark.parametrize("is_cuda_graph_compatible", [True, False])
-def test_cudnn_decode(
+from tests.test_helpers.test_helpers import clear_cuda_cache
+from tests.attention.test_deepseek_mla import generate_kv_from_cache, attention_ref
+from flashinfer.utils import (
+    is_sm90a_supported,
+)
+# Layer: Decode MLA -> Not supported for trtllm backend, use fa2
+@pytest.mark.parametrize("batch_size", [1, 3, 5, 7, 157])
+@pytest.mark.parametrize("kv_len", [1, 17, 33, 96, 97, 114, 514, 1024])
+@pytest.mark.parametrize("qo_len", [1])
+@pytest.mark.parametrize("num_heads", [16])
+@pytest.mark.parametrize("causal", [False, True])
+@pytest.mark.parametrize("page_size", [1, 16])
+@pytest.mark.parametrize("backend", ["fa2"])
+@pytest.mark.parametrize("use_cuda_graph", [False])
+@pytest.mark.parametrize("dtype", [torch.bfloat16])
+def test_batch_mla_page_attention(
     batch_size,
-    s_kv,
+    kv_len,
+    qo_len,
+    num_heads,
+    causal,
     page_size,
-    num_kv_heads,
-    num_qo_heads,
-    is_cuda_graph_compatible,
+    backend,
+    use_cuda_graph,
+    dtype,
 ):
-    # test set up basics
-    seed = 0
-    torch.manual_seed(seed)
-    device = "cuda:0"
-
-    s_qo = 1
-    head_dim = 128
-
-    # Initialize Q tensor
-    # Since the number of tokens is 1, batch size is the token count
-    q = torch.randn(
-        batch_size, num_qo_heads, head_dim, device=device, dtype=torch.bfloat16
+    device = torch.device("cuda:0")
+    clear_cuda_cache(device)
+    if backend == "fa3" and not is_sm90a_supported(device):
+        pytest.skip("FA3 is not supported on this device")
+    if causal and qo_len > kv_len:
+        pytest.skip("qo_len > kv_len not supported for causal attention")
+    torch.manual_seed(42)
+    head_dim_ckv = 512
+    head_dim_kpe = 64
+    q_nope = torch.randn(
+        batch_size * qo_len, num_heads, head_dim_ckv, dtype=dtype, device=device
     )
-
-    # Initialize KV Cache
-    num_pages_per_seq = (s_kv + page_size - 1) // page_size
-    total_num_pages = num_pages_per_seq * batch_size
-
-    kv_cache_shape = (total_num_pages, 2, num_kv_heads, page_size, head_dim)
-    kv_cache = torch.randn(size=kv_cache_shape, dtype=torch.bfloat16).to(device)
-    kv_cache = kv_cache.as_strided(
-        kv_cache.shape,
-        (
-            2 * page_size * num_kv_heads * head_dim,
-            page_size * num_kv_heads * head_dim,
-            head_dim,
-            num_kv_heads * head_dim,
-            1,
-        ),
+    q_pe = torch.randn(
+        batch_size * qo_len, num_heads, head_dim_kpe, dtype=dtype, device=device
     )
-    k_cache_view = kv_cache[:, 0, :, :, :]
-    v_cache_view = kv_cache[:, 1, :, :, :]
-
-    v_cache = v_cache_view.as_strided(
-        v_cache_view.shape,
-        (2 * page_size * num_kv_heads * head_dim, head_dim, num_kv_heads * head_dim, 1),
-    )
-    k_cache = k_cache_view.as_strided(
-        k_cache_view.shape,
-        (2 * page_size * num_kv_heads * head_dim, head_dim, num_kv_heads * head_dim, 1),
-    )
-
-    # Now initialize the page tables
-    block_tables = torch.tensor(
-        [
-            [k + i * num_pages_per_seq for k in range(num_pages_per_seq)]
-            for i in range(batch_size)
-        ],
-        dtype=torch.int,
+    pages_num = math.ceil(kv_len / page_size)
+    ckv = torch.randn(
+        batch_size * pages_num,
+        page_size,
+        head_dim_ckv,
+        dtype=dtype,
         device=device,
     )
-
-    # Initialize scale
-    scale = float(1.0 / (head_dim**0.5))
-
-    # Actual sequence lengths (should be randomized across batches. )
-    actual_seq_lens_kv = torch.randint(
-        0, s_kv + 1, (batch_size, 1, 1, 1), dtype=torch.int32, device=device
+    kpe = torch.randn(
+        batch_size * pages_num,
+        page_size,
+        head_dim_kpe,
+        dtype=dtype,
+        device=device,
     )
-
-    ragged_q = torch.arange(0, batch_size + 1, device=device) * (
-        num_qo_heads * head_dim
-    )
-
-    workspace_buffer_size = math.ceil(
-        (
-            batch_size * s_qo * num_qo_heads * head_dim * 4
-            + batch_size * s_qo * num_qo_heads * 4
-        )
-        / (1024 * 1024)
-    ) * (1024 * 1024)
-
-    workspace_buffer_size = max(workspace_buffer_size, 128 * 1024 * 1024)
-
-    workspace_buffer = torch.empty(
-        workspace_buffer_size, dtype=torch.int8, device=device
-    )
-
-    output = flashinfer.decode.cudnn_batch_decode_with_kv_cache(
-        q,
-        k_cache,
-        v_cache,
-        scale,
+    sm_scale = 1.0 / ((128 + 64) ** 0.5)  # use head dimension before matrix absorption
+    workspace_buffer = torch.empty(128 * 1024 * 1024, dtype=torch.int8, device=device)
+    wrapper = flashinfer.mla.BatchMLAPagedAttentionWrapper(
         workspace_buffer,
-        max_sequence_kv=s_kv,
-        actual_seq_lens_kv=actual_seq_lens_kv,
-        block_tables=block_tables,
-        is_cuda_graph_compatible=is_cuda_graph_compatible,
-        batch_offsets_q=ragged_q,
-        batch_offsets_o=ragged_q,
+        backend=backend,
+        use_cuda_graph=True,
+        qo_indptr=torch.empty(batch_size + 1, dtype=torch.int32, device=device),
+        kv_indptr=torch.empty(batch_size + 1, dtype=torch.int32, device=device),
+        kv_indices=torch.empty(1048576, dtype=torch.int32, device=device),
+        kv_len_arr=torch.empty(batch_size, dtype=torch.int32, device=device),
     )
-
-    actual_seq_lens_kv_device = actual_seq_lens_kv.to(device)
-
+    q_indptr = (
+        torch.arange(0, batch_size + 1, device=device, dtype=torch.int32) * qo_len
+    )
     kv_indptr = (
-        torch.cat(
-            [
-                torch.tensor([0], device=device),
-                torch.cumsum(
-                    (actual_seq_lens_kv_device.flatten() + page_size - 1) // page_size,
-                    dim=0,
-                ),
-            ]
-        )
-        .int()
-        .to(device)
+        torch.arange(0, batch_size + 1, device=device, dtype=torch.int32) * pages_num
     )
+    kv_indices = torch.arange(
+        0, batch_size * pages_num, device=device, dtype=torch.int32
+    )
+    kv_lens = torch.full((batch_size,), kv_len, dtype=torch.int32, device=device)
 
-    # kv_indices
-    kv_indices = torch.zeros(kv_indptr[-1], device=device, dtype=torch.int32)
-    for i in range(len(kv_indptr) - 1):
-        start_idx = kv_indptr[i]
-        end_idx = kv_indptr[i + 1]
-        kv_indices[start_idx:end_idx] = torch.arange(
-            i * num_pages_per_seq,
-            i * num_pages_per_seq + (end_idx - start_idx),
-            device=device,
+    if use_cuda_graph:
+        kv_indptr_warmup = torch.zeros(batch_size + 1, device=device, dtype=torch.int32)
+        kv_indices_warmup = torch.arange(
+            0, batch_size, device=device, dtype=torch.int32
+        )
+        kv_lens_warmup = torch.full((batch_size,), 0, dtype=torch.int32, device=device)
+        wrapper.plan(
+            q_indptr,
+            kv_indptr_warmup,
+            kv_indices_warmup,
+            kv_lens_warmup,
+            num_heads,
+            head_dim_ckv,
+            head_dim_kpe,
+            page_size,
+            causal,
+            sm_scale,
+            q_nope.dtype,
+            ckv.dtype,
         )
 
-    # kv_last_page_len
-    kv_last_page_len = (
-        torch.where(
-            actual_seq_lens_kv_device.flatten() % page_size == 0,
-            torch.full((batch_size,), page_size, device=device),
-            actual_seq_lens_kv_device.flatten() % page_size,
-        )
-        .int()
-        .to(device)
-    )
+        # warmup
+        s = torch.cuda.Stream()
+        s.wait_stream(torch.cuda.current_stream())
+        with torch.cuda.stream(s):
+            for _ in range(3):
+                o, lse = wrapper.run(q_nope, q_pe, ckv, kpe, return_lse=True)
+        torch.cuda.current_stream().wait_stream(s)
 
-    # Workspace buffer
-    workspace_buffer_ref = torch.empty(
-        128 * 1024 * 1024, dtype=torch.int8, device=device
-    )
+        # capture
+        g = torch.cuda.CUDAGraph()
+        with torch.cuda.graph(g):
+            o, lse = wrapper.run(q_nope, q_pe, ckv, kpe, return_lse=True)
 
-    wrapper = flashinfer.BatchDecodeWithPagedKVCacheWrapper(workspace_buffer_ref, "HND")
     wrapper.plan(
+        q_indptr,
         kv_indptr,
         kv_indices,
-        kv_last_page_len,
-        num_qo_heads,
-        num_kv_heads,
-        head_dim,
+        kv_lens,
+        num_heads,
+        head_dim_ckv,
+        head_dim_kpe,
         page_size,
-        q_data_type=torch.bfloat16,
+        causal,
+        sm_scale,
+        q_nope.dtype,
+        ckv.dtype,
     )
+    if use_cuda_graph:
+        o.fill_(0)
+        lse.fill_(0)
+        g.replay()
+    else:
+        o, lse = wrapper.run(q_nope, q_pe, ckv, kpe, return_lse=True)
 
-    output_ref = wrapper.run(q, kv_cache)
+    k, v = generate_kv_from_cache(ckv, kpe, kv_len, batch_size, num_heads)
 
-    torch.testing.assert_close(output, output_ref, rtol=1e-2, atol=1e-2)
+    q = torch.cat([q_nope, q_pe], dim=-1)
+    o_ref, lse_ref = attention_ref(batch_size, q, k, v, causal, sm_scale)
+    lse_ref = lse_ref.flatten(0, 1)
+    if dtype == torch.bfloat16:
+        torch.testing.assert_close(o, o_ref, rtol=1e-2, atol=1e-2)
+    else:
+        torch.testing.assert_close(o, o_ref, rtol=1e-3, atol=1e-3)
+    if kv_len != 0:
+        torch.testing.assert_close(lse, lse_ref, rtol=1e-3, atol=1e-3)
+
+    # test with pre-allocated output
+    o_buffer = torch.empty_like(o)
+    lse_buffer = torch.empty_like(lse)
+    wrapper.run(q_nope, q_pe, ckv, kpe, out=o_buffer, lse=lse_buffer)
+    if dtype == torch.bfloat16:
+        torch.testing.assert_close(o, o_buffer, rtol=1e-2, atol=1e-2)
+    else:
+        torch.testing.assert_close(o, o_buffer, rtol=1e-3, atol=1e-3)
+    torch.testing.assert_close(lse, lse_buffer, rtol=1e-3, atol=1e-3)
 
 
 # Layer: Dense GEMM (Attention)
